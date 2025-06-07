f54bf32a663eab33e826f9d217362cd05806e145
Lexluthor <lexluthordevfull@gmail.com>
2025-06-07 14:09:51 -0300

    ngrok

--- src/components/MinesGame.jsx
+++ src/components/MinesGame.jsx
@@ -91,7 +91,7 @@ const MinesGame = ({ token_jogador }) => {
 
   useEffect(() => {
     // Create socket inside effect
-    socketRef.current = io("https://56bc-45-160-88-131.ngrok-free.app", {
+    socketRef.current = io("https://830c-45-160-88-131.ngrok-free.app", {
       transports: ["websocket"],
       auth: {
         token: token_jogador, // Certifique-se de passar o mesmo token usado na outra página
